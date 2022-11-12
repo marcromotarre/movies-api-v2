8968f587612e9aea26993017fb45abcac5866f9e
Marc Romo <marc.romo@attackiq.com>
2022-11-12 04:26:52 +0100

    mg

--- src/handlers/movie-gallery.ts
+++ src/handlers/movie-gallery.ts
@@ -1,35 +1,39 @@
 import prisma from "../db";
 
 export const getMoviesInGallery = async (req: any, res: any) => {
-  console.log("FIND MOVIE IN GALLERY");
-  const get = await prisma.movie.findMany({
-    where: {
-      platforms: {
-        imdbMovie: {
-          rating: { gt: 0 },
+  try {
+    const get = await prisma.movie.findMany({
+      where: {
+        platforms: {
+          imdbMovie: {
+            rating: { gt: 0 },
+          },
         },
       },
-    },
-    select: {
-      name: true,
-      image: true,
-      platforms: {
-        select: {
-          imdbMovie: {
-            select: {
-              rating: true,
+      select: {
+        name: true,
+        image: true,
+        platforms: {
+          select: {
+            imdbMovie: {
+              select: {
+                rating: true,
+              },
             },
           },
         },
       },
-    },
-    orderBy: {
-      platforms: {
-        imdbMovie: {
-          rating: "desc",
+      orderBy: {
+        platforms: {
+          imdbMovie: {
+            rating: "desc",
+          },
         },
       },
-    },
-  });
-  res.json({ data: get, errors: [] });
+    });
+    res.status(200)
+    res.json({ data: get, errors: [] });
+  } catch (e) {
+    res.json({ data: [], errors: [e] });
+  }
 };
