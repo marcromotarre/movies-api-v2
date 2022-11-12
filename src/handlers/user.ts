b5b171e840b223eb16c71b478bc2a8b23797721d
Marc Romo <marc.romo@attackiq.com>
2022-11-12 01:48:29 +0100

    some logs

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -6,15 +6,24 @@ import { createUserRankingEngines } from "./user-ranking-engines";
 
 export const createNewUser = async (req: any, res: any) => {
   try {
+    console.log("WE ARE IN CREATE USER")
+    console.log("email", req.body.email)
+    console.log("password", req.body.password)
     const user = await prisma.user.create({
       data: {
         email: req.body.email,
         password: await hashPassword(req.body.password),
       },
     });
+    console.log("USER HAS BEEN CREATED")
+
     const token = createJWT(user);
+    console.log("TOKEN", token)
+
+    res.status(200);
     res.json(token);
   } catch (e) {
+    console.log("OH NO WE GOT AN ERROR")
     res.status(500);
     res.json({ error: e });
   }
